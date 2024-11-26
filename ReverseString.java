975361d72b483951f551737bccbe56909a14ff68
Aliyan <bukharialian7@gmail.com>
2024-11-26 11:03:42 +0500

    reversestring modified

--- ReverseString.java
+++ ReverseString.java
@@ -1,12 +1,13 @@
 public class ReverseString {
     public static void main(String[] args) {
         String str = "Valar Morghulis";
-        String reversedStr = "";
+        String reverseStr = "";
 
         for (int i = str.length() - 1; i >= 0; i--) {
 
-            reversedStr += str.charAt(i);
+
+            reverseStr += str.charAt(i);
         }
-        System.out.println(reversedStr);
+        System.out.println(reverseStr);
     }
 }
